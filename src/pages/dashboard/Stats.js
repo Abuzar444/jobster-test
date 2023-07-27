6cdb060b9f4afd89e60ecda5e0fa4ae6f93ac96a
= <=>
2023-07-28 00:19:48 +0500

    commit

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,16 +1,17 @@
 import { useEffect } from 'react'
-import { StatsContainer, Loading, ChartContainer } from '../../components'
+import { StatsContainer, ChartContainer } from '../../components'
 import { useDispatch, useSelector } from 'react-redux'
 import { showStats } from '../../features/allJobs/allJobsSlice'
 
 const Stats = () => {
-  const { isLoading, monthlyApplications } = useSelector(
+  const { monthlyApplications } = useSelector(
     (store) => store.allJobs
   )
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(showStats())
+    // eslint-disable-next-line
   }, [])
   return (
     <>
